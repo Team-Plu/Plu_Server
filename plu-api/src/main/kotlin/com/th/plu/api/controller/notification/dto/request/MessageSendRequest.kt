77042969368f6c85869f99429629cd8b74e4fe3e
Sookyung Kang <thoosoo@naver.com>
2024-04-06 02:16:53 +0900

    ⚡️: fix 오늘의 질문 관련 오류 수정  #36

--- plu-api/src/main/kotlin/com/th/plu/api/controller/notification/dto/request/MessageSendRequest.kt
+++ plu-api/src/main/kotlin/com/th/plu/api/controller/notification/dto/request/MessageSendRequest.kt
@@ -2,7 +2,7 @@ package com.th.plu.api.controller.notification.dto.request
 
 data class MessageSendRequest(var title: String, var body: String) {
     companion object {
-        fun dailySendMessage() = MessageSendRequest(
+        fun dailyQuestionMessage() = MessageSendRequest(
             title = "오늘의 질문 알림",
             body = "오늘의 질문에 답할 시간입니다."
         )
